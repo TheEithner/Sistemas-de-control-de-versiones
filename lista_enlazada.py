964699c1c334a28b1e48085c0d19cc894b2bd40a
Eithner Palacios <eithnergpp@gmail.com>
2026-03-12 18:02:56 -0500

    Agregamos la clase lista enlazada y funcion vaciar

--- lista_enlazada.py
+++ lista_enlazada.py
@@ -1,9 +1,16 @@
 # Eithner Geovanny Palacios Palacios
-# Sistemas de control de versiones
+# Sistemas de control de versiones - 37
 
-class Node:
+class Nodo:
     def __init__(self):
         self.dato = None
         self.siguiente = None
 
-        
+class lista_enlazada:
+    def __init__(self):
+        self.cabeza = None
+
+    def vaciar(self):
+        return self.cabeza is None
+    
+    
